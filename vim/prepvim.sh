60b1610dbf8304c100f4896361124ba474e06db0
Bogdan Costea <bogdan.costea@arcticwolf.com>
2019-04-24 12:42:33 -0400

    vagrantfile syntax

--- vim/prepvim.sh
+++ vim/prepvim.sh
@@ -1,4 +1,5 @@
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-@echo installing nerd tree
+
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+git clone https://github.com/morhetz/gruvbox.git ~/.vim/bundle/gruvbox
