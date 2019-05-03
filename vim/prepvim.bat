460413eb02f3d89d15c058985a8c2ab9309e38fd
Bogdan Costea <bogdan.costea@arcticwolf.com>
2019-05-03 14:55:59 -0400

    add elixir syntax

--- vim/prepvim.bat
+++ vim/prepvim.bat
@@ -6,7 +6,7 @@ cd %USERPROFILE%\vimfiles\bundle
 git clone https://github.com/PProvost/vim-ps1.git
 git clone https://github.com/scrooloose/nerdtree.git
 git clone https://github.com/morhetz/gruvbox.git
-
+git clone https://github.com/elixir-lang/vim-elixir.git 
 cd ..
 
 curl -LSso autoload/pathogen.vim https://tpo.pe/pathogen.vim
