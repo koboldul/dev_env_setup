7dece1e455b5cabdd3c2569334fd27d4bc6dc546
Bogdan Costea <bogdancostea@microsoft.com>
2024-02-29 10:53:46 -0800

    d

--- vagrant/win_build_env/scripts/java_home.ps1
+++
@@ -1,5 +0,0 @@
-$javaPath = gci ("${env:ProgramFiles}\Java", "${env:ProgramFiles(x86)}\Java")[!(Test-Path "${env:ProgramFiles}\Java")] java.exe -Recurse | select -f 1 | Split-Path | Convert-Path
-$javaHome = $javaPath | Split-Path
-
-[Environment]::SetEnvironmentVariable("JAVA_HOME", $javaHome, "Machine")
-[Environment]::SetEnvironmentVariable("Path", $env:Path + ";" +  $javaPath, "Machine")
