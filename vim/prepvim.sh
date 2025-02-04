059a71ddf1e31ab71534adf032d99a654a2bb8b5
Bogdan Costea <bogdancostea@microsoft.com>
2025-02-04 15:01:38 -0500

    ai

--- vim/prepvim.sh
+++ vim/prepvim.sh
@@ -4,3 +4,6 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 git clone https://github.com/morhetz/gruvbox.git ~/.vim/bundle/gruvbox
 git clone https://github.com/elixir-lang/vim-elixir.git ~/.vim/bundle/vim-elixir
+
+cp .vimrc "$HOME"
+
